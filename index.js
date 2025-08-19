b0888ee1c15667e39bfe7872e6159551f99ad986
vivekkumarsoni123 <str.vivekkumarsoni123@gmail.com>
2025-08-19 22:25:30 +0530

    done with the setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from my Node DevOps Vivek');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
