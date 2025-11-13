d8e30066fea4750b01fc7f9eaeba515b7e23b94a
vivekkumarsoni123 <str.vivekkumarsoni123@gmail.com>
2025-11-13 14:25:42 +0530

    Fix greeting message typo in root route

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello everyone, this is vivek kumar son');
+  res.send('Hello everyone, this is vivek kumar soni');
 });
 
 app.listen(port, () => {
